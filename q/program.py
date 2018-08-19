f8f4a3d7cda354e01e9b98c1b79a086172259a0f
Vincent Wang <vwang00@gmail.com>
2018-08-19 23:10:38 +0100

    put q leaner logic in separate class and general cleanup

--- q/program.py
+++ q/program.py
@@ -16,71 +16,84 @@ discountFactor = 0.95
 roundNo = 30000
 modelNo = 0
 
-def display(qTable):
-    for y in range(0, stateNo):
-        for x in range(0, actionNo):
-            print(str(qTable[y][x]) + "\t", end = '')
-        print()
 
-    outs = ''
-    for i in range(0, stateNo):
-        if qTable[i][0] > qTable[i][1]:
-            outs += "0"
-        else:
-            outs += "1"
+#class for a Q learner
+class qLearner():
+
+    def __init__(self):
+        self.generate()
+        self.display()
     
-    print(outs)
 
-    print()
-
-
-#init the table with random rewards
-def generate():
-    qTable = []
-    for y in range(0, stateNo):
-        row = []
-        for x in range(0, actionNo):
-            #random float between 0, 1
-            row.append(r.uniform(0, 0.5))
-        qTable.append(row)
-    
-    return qTable
+    #init the table with random rewards
+    def generate(self):
+        qTable = []
+        for y in range(0, stateNo):
+            row = []
+            for x in range(0, actionNo):
+                #random float between 0, 1
+                row.append(r.uniform(0, 0.5))
+            qTable.append(row)
+        self.qTable = qTable
 
 
-#find correct state (row in table)
-def getState(history):
-    state = 0
-    #if not first round
-    if len(history[0]) > 0:
-        state = 1 + (2 * history[0][-1]) + history[1][-1]
-    return state
+    #diplay rewards and compact strategy
+    def display(self):
+        for y in range(0, stateNo):
+            for x in range(0, actionNo):
+                print(str(self.qTable[y][x]) + "\t", end = '')
+            print()
+
+        outs = ''
+        for i in range(0, stateNo):
+            if self.qTable[i][0] > self.qTable[i][1]:
+                outs += "0"
+            else:
+                outs += "1"        
+        print(outs)
 
 
-#choose action with greatest reward (column in table)
-def getAction(row):
-    largest = row[0]
-    index = 0
-    for i in range(1, actionNo):
-        if row[i] > largest:
-            largest = row[i]
-            index = i
-    return index
+    #use update rule
+    def updateTable(self, s, a, sp, r):
+        r = r/10
+        oldEstimate = (1-learningRate) * self.qTable[s][a]
+        newEstimate = learningRate * (r + (discountFactor * self.qTable[sp][self.getAction(sp)]))
+        self.qTable[s][a] = oldEstimate + newEstimate
+        return
 
 
-#use update rule
-def updateTable(qTable, s, a, sp, r):
-    r = r/10
-    oldEstimate = (1-learningRate) * qTable[s][a]
-    newEstimate = learningRate * (r + (discountFactor * qTable[sp][getAction(qTable[sp])]))
-    
-    qTable[s][a] = oldEstimate + newEstimate
-
-    #use update rule shown in "Multiagent reinforcement learning in the Iterated Prisoner's Dilemma"
-    #qTable[s][a] += learningRate * (r + (discountFactor * qTable[sp][getAction(qTable[sp])]) - qTable[s][a])
-    return
+    #find correct state (row in table)
+    def getState(self, history):
+        state = 0
+        #if not first round
+        if len(history[0]) > 0:
+            state = 1 + (2 * history[0][-1]) + history[1][-1]
+        return state
 
 
-def play(qTable, opponent):
+    #choose action with greatest reward (column in table)
+    def getAction(self, index):
+        largest = self.qTable[index][0]
+        index = 0
+        for i in range(1, actionNo):
+            if self.qTable[index][i] > largest:
+                largest = self.qTable[index][i]
+                index = i
+        return index
+
+
+
+#switch player move as a function of roundNo - more exploration at start
+def tryExplore(playerMove, n):
+    toss = r.uniform(0, 1)
+    if toss < (40 - (n/100))/100:
+        playerMove = abs(playerMove-1)
+    return playerMove
+
+
+
+#play roundNo rounds against an opponent
+def play(learner, opponent):
     oppHistory = []
     playerHistory = []
     #generate player
@@ -89,59 +102,46 @@ def play(qTable, opponent):
     #initial state
     state = 0
 
+
     for i in range(0, roundNo):
-          
+
         oppMove = opponent.move([oppHistory, playerHistory])
-        playerMove = getAction(qTable[state])
+        playerMove = learner.getAction(state)
 
-        #switch player move as a function of roundNo - more exploration at start
-        toss = r.uniform(0, 1)
-        if toss < (40 - (i/100))/100:
-            playerMove = abs(playerMove-1)
-            #print(str(i) + " " + str(toss) + str(playerMove))
+        #toss coin to see if agent explores
+        playerMove = tryExplore(playerMove, i)
 
+        #update history
         oppHistory.append(oppMove)
         playerHistory.append(playerMove)
 
         #observe rewawrd and new state
-        newState = getState([playerHistory, oppHistory])
+        newState = learner.getState([playerHistory, oppHistory])
         reward = player.payoff(playerMove, oppMove)
 
-
-        updateTable(qTable, state, playerMove, newState, reward)
-
-
+        #update table
+        learner.updateTable(state, playerMove, newState, reward)
+        
         state = newState
 
 
-        #print("Player: " + str(playerMove) + " " + opponent.strategy + ": " + str(oppMove))
-
-    #print("\nPlayer Score: " + str(player.score) + "\n\n")
-
-    return player.score
-
-
+#entry point
 def Main():
+    
     strategies = [prisoner.strategies[3]]
 
-    allModels = []
-
-    #policy mapping
-    qTable = generate()
-    display(qTable)
+    #init a learner to use
+    learner = qLearner()
     
-
     for i in range(0, iterations):        
         #change order of strategies
         r.shuffle(strategies)
-
         for n in range(0, len(strategies)):
             #play strategy and update table
-            play(qTable, prisoner.player(strategies[n]))
+            play(learner, prisoner.player(strategies[n]))
 
     #display results
-    display(qTable)
-            
+    learner.display()            
 
 
 
